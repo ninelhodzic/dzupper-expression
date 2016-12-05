ea20d67545facb2080fcd771bab7399827713ae3
Ninel Hodzic <ninel.hodzic@gmail.com>
2016-12-05 01:06:15 +0100

    small test for other types

--- src/test/java/base/OtherTest.java
+++ src/test/java/base/OtherTest.java
@@ -2,8 +2,12 @@ package base;
 
 import junit.framework.Assert;
 import org.datazup.utils.DateTimeUtils;
+import org.datazup.utils.JsonUtils;
 import org.joda.time.DateTime;
 import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by ninel on 11/30/16.
@@ -15,5 +19,20 @@ public class OtherTest {
         Object twitterCreatedAt = "Wed Aug 27 13:08:45 +0900 2008";
         DateTime dt = DateTimeUtils.resolve(twitterCreatedAt);
         Assert.assertNotNull(dt);
+    }
+
+    @Test
+    public void isJsonKeyValid(){
+        Map<String,Object> map = new HashMap<>();
+
+        map.put("ime~prezime", 123);
+        map.put("ime#prezime", 234);
+        map.put("ime%prezime", 456);
+        map.put("ime.prezime", 567);
+        String json = JsonUtils.getJsonFromObject(map);
+
+        Map<String,Object> map1 = JsonUtils.getMapFromJson(json);
+
+        System.out.println(JsonUtils.getJsonFromObject(map1));
     }
 }
